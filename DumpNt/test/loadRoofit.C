c0845513fb5551a3d7ac8e8de75b961d5d554d66
sandro <>
2012-09-24 17:30:25 +0000

    This commit was manufactured by cvs2svn to create tag 'V03-02-16'.
    Sprout from master 2012-09-24 17:30:23 UTC sandro '*** empty log message ***'
    Delete:
        DumpNt/BuildFile.xml
        DumpNt/data/0_Original_MC.txt
        DumpNt/data/1_PixelBarrelConnToST_MC.txt
        DumpNt/data/2_PixelBarrelConn1_and_PixelBarrelConn2_MC.txt
        DumpNt/data/3_TIBFlange_MC.txt
        DumpNt/data/4_TIBTIDServiceCylinder_MC.txt
        DumpNt/data/5_TIBTIDMargherita_MC.txt
        DumpNt/data/6_Geometries_12345_MC.txt
        DumpNt/data/data_replica.py
        DumpNt/data/makelist.sh
        DumpNt/python/dumpnt_cfi.py
        DumpNt/src/DumpNt.cc
        DumpNt/test/All_Draw_fit.C
        DumpNt/test/Draw_fit.C
        DumpNt/test/dumpnt_cfg.py
        DumpNt/test/fit.C
        DumpNt/test/gbrv3ele_52x.root
        DumpNt/test/loadRoofit.C
        DumpNt/test/rmfromT2.sh
        MaterialBudget/test/BeamPipe
        MaterialBudget/test/Divide.C
        MaterialBudget/test/EtaPhiMap.C
        MaterialBudget/test/InnerServices
        MaterialBudget/test/PixBar
        MaterialBudget/test/PixFwdMinus
        MaterialBudget/test/PixFwdPlus
        MaterialBudget/test/Subtract.C
        MaterialBudget/test/TEC
        MaterialBudget/test/TIB
        MaterialBudget/test/TIDB
        MaterialBudget/test/TIDF
        MaterialBudget/test/TOB
        MaterialBudget/test/TkStrct

--- DumpNt/test/loadRoofit.C
+++
@@ -1,6 +0,0 @@
-{
-gSystem->Load("/afs/cern.ch/cms/slc5_amd64_gcc462/lcg/roofit/5.32.00-cms6/lib/libRooFit.so");
-gSystem->Load("/afs/cern.ch/cms/slc5_amd64_gcc462/lcg/roofit/5.32.00-cms6/lib/libRooFitCore.so");
-using namespace RooFit;
-gSystem->AddIncludePath("-I/afs/cern.ch/cms/slc5_amd64_gcc462/lcg/roofit/5.32.00-cms6/include");
-}
